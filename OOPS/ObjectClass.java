20c45ea88bb5a8f14ec1818a7decf0cf855dd7d3
khushdj2002 <108506287+khushdj2002@users.noreply.github.com>
2023-02-02 12:35:15 +0530

    a

---
+++ OOPS/ObjectClass.java
@@ -0,0 +1,9 @@
+package OOPS;
+
+ class ObjectClass {
+    public static void main(String[] args) {
+        Student s1  = new Student();
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+    }
+}
---
+++ OOPS/Student.java
@@ -0,0 +1,6 @@
+package OOPS;
+
+public class Student {
+    int id;
+    String name;
+}
